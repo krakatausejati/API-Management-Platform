c7450505ffce6915b40271fa9c6d14250b9b8a82
softone-as <shofwanas12@gmail.com>
2022-04-22 11:07:22 +0700

    adding POST Connection Service

--- src/pages/Connection/Connection.js
+++ src/pages/Connection/Connection.js
@@ -1,12 +1,15 @@
 import { PlusOutlined } from "@ant-design/icons";
-import { Button, Table } from "antd";
-import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Button, Form, Input, Modal, Table } from "antd";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { Breadcrumbs } from "../../components/molecules/Breadcrumbs";
 import useConnection from "../../hooks/useConnection";
+import { ConnectionService } from "../../services/ConnectionService";
 
 function Connection() {
 	const connection = useConnection();
+	const [isModalVisible, setIsModalVisible] = useState(false);
+	const [form] = Form.useForm();
 
 	const dataSource = connection.map((connectionItem, index) => ({
 		key: `${connectionItem.id}`,
@@ -63,13 +66,152 @@ function Connection() {
 		},
 	];
 
+	const showModal = () => {
+		setIsModalVisible(true);
+	};
+
+	const handleOk = (values) => {
+		ConnectionService.createConnection(values)
+			.then((response) => {
+				window.location.reload();
+			})
+			.catch((error) => {
+				console.log("Something went wrong", error);
+			});
+
+		setIsModalVisible(false);
+	};
+
+	const handleCancel = () => {
+		setIsModalVisible(false);
+	};
+
 	return (
 		<>
+			{/* Modal */}
+			<Modal
+				title='Add New Connection'
+				visible={isModalVisible}
+				onOk={() => {
+					form.validateFields()
+						.then((values) => {
+							form.resetFields();
+							handleOk(values);
+						})
+						.catch((info) => {
+							console.log("Validate Failed:", info);
+						});
+				}}
+				onCancel={handleCancel}
+				okText='Add Connection'
+				cancelText='Cancel'
+			>
+				<Form layout='vertical' form={form}>
+					<div
+						className='form-container-connection'
+						style={{ display: "flex", gap: "5rem" }}
+					>
+						<div className='left-side'>
+							<Form.Item
+								label='Connection Name'
+								name='connectionName'
+								style={{ width: "100%" }}
+								rules={[
+									{
+										required: true,
+										message:
+											"Please input the connection name!",
+									},
+								]}
+							>
+								<Input />
+							</Form.Item>
+							<Form.Item
+								label='Host'
+								name='host'
+								style={{ width: "100%" }}
+								rules={[
+									{
+										required: true,
+										message: "Please input the host!",
+									},
+								]}
+							>
+								<Input />
+							</Form.Item>
+							<Form.Item
+								label='Port'
+								name='port'
+								style={{ width: "100%" }}
+								rules={[
+									{
+										required: true,
+										message: "Please input the port!",
+									},
+								]}
+							>
+								<Input />
+							</Form.Item>
+						</div>
+						<div className='right'>
+							<Form.Item
+								label='Database Name'
+								name='databaseName'
+								style={{ width: "100%" }}
+								rules={[
+									{
+										required: true,
+										message:
+											"Please input the database name!",
+									},
+								]}
+							>
+								<Input />
+							</Form.Item>
+							<Form.Item
+								label='Database Username'
+								name='databaseUserame'
+								style={{ width: "100%" }}
+								rules={[
+									{
+										required: true,
+										message:
+											"Please input the database username!",
+									},
+								]}
+							>
+								<Input />
+							</Form.Item>
+							<Form.Item
+								label='Database Password'
+								name='databasePassword'
+								style={{ width: "100%" }}
+								rules={[
+									{
+										required: true,
+										message:
+											"Please input the database password!",
+									},
+								]}
+							>
+								<Input />
+							</Form.Item>
+						</div>
+					</div>
+				</Form>
+			</Modal>
+			{/* Modal */}
+
 			<Breadcrumbs breadcrumb='Connection' current={null} />
 			<div className='header-datatable'>
 				<h1>List Connections</h1>
 				<div className='add-field'>
-					<Button icon={<PlusOutlined />} type='primary' block>
+					<Button
+						icon={<PlusOutlined />}
+						type='primary'
+						block
+						onClick={showModal}
+					>
 						Add Connection
 					</Button>
 				</div>
--- src/services/ConnectionService.js
+++ src/services/ConnectionService.js
@@ -3,8 +3,31 @@ import { BASE_URL } from "../helpers/Constant";
 
 export const ConnectionService = {
 	getAllConnection,
+	createConnection,
 };
 
 function getAllConnection() {
 	return axiosInstance(BASE_URL.CONNECTION).get("");
+}
+
+function createConnection(values) {
+	const {
+		connectionName,
+		host,
+		port,
+		databaseName,
+		databaseUsername,
+		databasePassword,
+	} = values;
+
+	let connectionData = {
+		connectionName,
+		host,
+		port,
+		databaseName,
+		databaseUsername,
+		databasePassword,
+	};
+
+	return axiosInstance(BASE_URL.CONNECTION).post("", connectionData);
 }
