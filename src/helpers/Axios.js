7f9865aa34c835d059931a16b278cd3e4d212200
softone-as <shofwanas12@gmail.com>
2022-06-16 16:45:34 +0700

    Resolve expired token

--- src/helpers/Axios.js
+++ src/helpers/Axios.js
@@ -14,26 +14,11 @@ const axiosInstance = (url = "") => {
 			const accessToken = localStorage.getItem("access_token");
 			const accessTokenExpired =
 				localStorage.getItem("accessTokenExpire");
-			// const limitExpire =
-			// 	new Date().getTime() + accessTokenExpired * 1000;
 			const currentTime = new Date().getTime();
 
 			if (accessToken) {
 				if (accessTokenExpired <= currentTime) {
-					console.log("truez");
-					const rs = await AuthService.refreshToken(accessToken);
-					console.log("true");
-					const { access_token, refresh_token, expires_in } = rs;
-					localStorage.setItem("access_token", access_token);
-					localStorage.setItem("refresh_token", refresh_token);
-					localStorage.setItem(
-						"accessTokenExpire",
-						expires_in * 1000 + new Date().getTime()
-					);
-					config.headers["Authorization"] = `Bearer ${access_token}`;
-				} else {
-					console.log();
-					config.headers["Authorization"] = `Bearer ${accessToken}`;
+					AuthService.logout();
 				}
 			}
 			return config;
